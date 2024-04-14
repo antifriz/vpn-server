48c14ebe5335c711dd65ad1ad963b0b016802872
Ivan Jurin <antifriz@users.noreply.github.com>
2024-04-15 00:42:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ wget https://dmej8g5cpdyqd.cloudfront.net/downloads/noip-duc_3.0.0.tar.gz
 
 tar xf noip-duc_3.0.0.tar.gz
 
-apt-get install -y noip-duc_3.0.0/binaries/noip-duc_3.0.0_armhf.deb
+sudo apt-get install -y noip-duc_3.0.0/binaries/noip-duc_3.0.0_armhf.deb
